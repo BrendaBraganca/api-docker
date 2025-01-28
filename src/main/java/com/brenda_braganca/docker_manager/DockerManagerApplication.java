23a9723a41a5e6e6bcc566f4a53a7292d34c1394
brenda_braganca <bragancadeassis@gmail.com>
2025-01-28 15:07:25 -0300

    feat: firt commit

---
+++ src/main/java/com/brenda_braganca/docker_manager/DockerManagerApplication.java
@@ -0,0 +1,13 @@
+package com.brenda_braganca.docker_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brenda_braganca/docker_manager/DockerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brenda_braganca.docker_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
